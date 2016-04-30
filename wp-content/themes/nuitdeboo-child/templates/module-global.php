69731fd43053da43291c73bd7c1ddd02c33e17a5
Alexandre Segura <mex.zktk@gmail.com>
2016-04-30 02:12:18 +0200

    WIP

--- wp-content/themes/nuitdeboo-child/templates/module-global.php
+++ wp-content/themes/nuitdeboo-child/templates/module-global.php
@@ -1,38 +1,63 @@
-<div id="global" class="section">
-	<?php
-	$page_parent = get_field('homepage_global', 'option');
-	if($page_parent->ID){
-		$parent_id = $page_parent->ID;
-		$exclude = false;
-		if(is_page_template('page-globaldebout.php') ){
-			$title ='Other languages / Autres langues';
-			$permalink = '';
-			$excerpt = '';
-			$exclude = $post->ID;
-		}
-		else{
-			$include = get_pages('include='.$parent_id);
-			$title   = apply_filters('the_title',$include[0]->post_title);
-			$permalink = get_permalink($include[0]->ID);
-			$excerpt   = get_the_excerpt($include[0]->ID);
-		}
-		$args = array(
-			'child_of' => $parent_id,
-			'post_type' => 'page',
-			'post_status' => 'publish',
-			'sort_order' => 'asc',
-			'sort_column' => 'menu_order',
-			'exclude' => $exclude
-		);
-		$pages_sub = get_pages($args);
+<?php
+$page_parent = get_field('homepage_global', 'option');
+if($page_parent->ID){
+	$parent_id = $page_parent->ID;
+	$exclude = false;
+	if(is_page_template('page-globaldebout.php') ){
+		$title ='Other languages / Autres langues';
+		$permalink = '';
+		$excerpt = '';
+		$exclude = $post->ID;
 	}
-	?>
-	<h2 class="section__title"><?php echo $title; ?></h2>
-	<div class="section__content">
+	else{
+		$include = get_pages('include='.$parent_id);
+		$title   = apply_filters('the_title',$include[0]->post_title);
+		$permalink = get_permalink($include[0]->ID);
+		$excerpt   = get_the_excerpt($include[0]->ID);
+	}
+	$args = array(
+		'child_of' => $parent_id,
+		'post_type' => 'page',
+		'post_status' => 'publish',
+		'sort_order' => 'asc',
+		'sort_column' => 'menu_order',
+		'exclude' => $exclude
+	);
+	$pages_sub = get_pages($args);
+}
+?>
+<div id="global" class="row">
+	<div class="global__text col-xs-12 col-sm-6">
+	<?php echo $excerpt; ?>
+	</div>
+	<div class="col-xs-12 col-sm-6">
+		<ul class="list-unstyled">
+		<?php
+		if ($pages_sub) :
+			$cols = 3;
+			$per_col = ceil(count($pages_sub) / $cols);
+			foreach ( $pages_sub as $p ) :
+				$content = apply_filters('the_content',$p->post_content);
+				$title = apply_filters('the_title',$p->post_title);
+				$permalink = get_permalink($p->ID);
+				$label = $title;
+				$page_lang_code = get_field('page_lang_code',$p->ID );
+				if($page_lang_code){
+					$label = $page_lang_code;
+				}
+				?>
+				<li><a href="<?php echo $permalink; ?>"><?php echo $page_lang_code; ?></a></li>
+			<?php endforeach;
+		endif;
+		?>
+		</ul>
+	</div>
+	<!-- <h2 class="section__title"><?php echo $title; ?></h2> -->
+	<!-- <div class="section__content">
 		<p><?php echo $excerpt; ?></p>
 		<ul class="cities-list">
 		<?php
-		if($pages_sub){
+		if ($pages_sub) :
 			foreach ( $pages_sub as $p ) :
 				$content = apply_filters('the_content',$p->post_content);
 				$title = apply_filters('the_title',$p->post_title);
@@ -45,8 +70,8 @@
 				?>
 				<li class="tag with-lang"><a href="<?php echo $permalink; ?>"><?php echo $page_lang_code; ?></a></li>
 			<?php endforeach;
-		}
+		endif;
 		?>
 		</ul>
-	</div>
+	</div> -->
 </div>
